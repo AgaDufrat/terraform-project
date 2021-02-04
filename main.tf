a10c9df359ed3d4726f37255689d0d75538931f0
Aga Dufrat <a.dufrat@gmail.com>
2021-02-04 22:23:10 +0000

    Add port 80 for HTTP and port 443 for HTTPS
    open to any IP address

--- main.tf
+++ main.tf
@@ -51,10 +51,25 @@ resource "aws_security_group" "sg_22" {
   name = "sg_22"
   vpc_id = aws_vpc.vpc.id
   ingress {
+    description = "SSH from any IP"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+  description = "HTTPS"
+  from_port   = 443
+  to_port     = 443
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
     from_port   = 0
